388a8d5bd36cd3d7e60a07bd7fe875f1b347fbac
ChrisTolve <christolve61@gmail.com>
2015-07-09 17:54:12 +0000

    percentage

---
+++ percentage.rb
@@ -0,0 +1,10 @@
+
+puts "enter a number to take a percentage of"
+number = gets.chomp
+
+puts "enter a percentage to take out oi that number"
+percentage = gets.chomp
+
+decimal = (percentage.to_f/100).to_f
+final_percent = decimal * (number.to_i)
+puts final_percent
